bd0c1db76e8c7e2a326c2e780be20cf6130ecbd4
straightupjac <jdc.jaclyn@gmail.com>
2022-09-12 12:23:09 -0700

    fix eslint

--- next.config.js
+++ next.config.js
@@ -1,7 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+  eslint: {
+    dirs: ['pages', 'components', 'utils', 'styles'],
+  },
+
   reactStrictMode: true,
   swcMinify: true,
-};
+  // Uncoment to add domain whitelist
+  // images: {
+  //   domains: [
+  //     'res.cloudinary.com',
+  //   ],
+  // },
 
-module.exports = nextConfig;
+  // SVGR
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            typescript: true,
+            icon: true,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
